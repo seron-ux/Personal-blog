4ade17728487dfe6005e88c6e1a8cd12b0dfea85
seron-ux <abyseronkirui@gmail.com>
2021-03-08 23:48:13 +0300

    debug the python files

--- config.py
+++ config.py
@@ -19,15 +19,15 @@ class Config:
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog_test'
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog_test'
 
-class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog'
-    DEBUG = True
+# class DevConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog'
+#     DEBUG = True
 
-config_options = {
-'development':DevConfig,
-'production':ProdConfig,
-'test':TestConfig
-}
+# config_options = {
+# 'development':DevConfig,
+# 'production':ProdConfig,
+# 'test':TestConfig
+# }
