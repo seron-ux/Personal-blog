b040ac003919bab504ac13be6602297e5631368b
seron-ux <abyseronkirui@gmail.com>
2021-03-08 23:34:16 +0300

    initialized application

--- app/models.py
+++ app/models.py
@@ -1,14 +1,14 @@
-# from . import db
-# from werkzeug.security import generate_password_hash,check_password_hash
-# from flask_login import UserMixin
-# from . import login_manager
-# from datetime import datetime
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-# class User(db.Model,UserMixin):
+class User(db.Model,UserMixin):
     __tablename__ = 'users'
     
     id=db.Column(db.Integer,primary_key=True)
