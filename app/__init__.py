ca21b22e357a1d07337962fbf4dd5dd29042db6a
seron-ux <abyseronkirui@gmail.com>
2021-03-08 23:17:12 +0300

    form a blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -37,11 +37,11 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    # from .auth import auth as auth_blueprint
+    # app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     
-    # configure UploadSet
-    configure_uploads(app,photos)
+    # # configure UploadSet
+    # configure_uploads(app,photos)
 
-    return app
+    # return app
